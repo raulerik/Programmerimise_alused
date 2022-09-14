e16891e5d7793c9174e7551c01582679650abef0
raulerik <raul.erik.laur@gmail.com>
2022-09-14 11:42:46 +0300

    Variables

--- datatypes/app.js
+++ datatypes/app.js
@@ -1,10 +1,18 @@
 /**
- * comment
- * style
+ * data types
+ *
+ * primitives - integer,
+ *
+ * variables - var, let, const
+ * let variableName = value // mutable
+ *
  */
 
 // this is a simple comment
-
+let numberOne = 5;
+let numberTwo = 2.5;
+const season = "Autumn"
+console.log(numberOne+numberTwo);
 console.log("Hello World!");
 console.log("second row");
 console.log("third row");
