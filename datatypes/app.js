da2eea2db7608f0640ee7921ca5ed30b6a290118
raulerik <raul.erik.laur@gmail.com>
2022-09-29 12:35:27 +0300

    Ülesanne 3.1

--- datatypes/app.js
+++ datatypes/app.js
@@ -1,39 +1,15 @@
-let vanus = prompt("Sisestage enda vanus:");
-let sugu = prompt("Sisestage enda sugu:");
-let treening = prompt("Sisestage treeningu tüüp:");
+// kasutaja sisend
+let kordamise_arv = Number(prompt("Sisestage mitu korda äratada"));
 
+let kord = 1;
 
-let pulsisagedusMax;
+// korduvad tegevused
 
-if(sugu == "n" || sugu == "N") {
-        pulsisagedusMax = 206 - 0.88 * vanus;
-}
-if(sugu == "m" || sugu == "M") {
-        pulsisagedusMax = 220 - vanus;
+while(kord <= kordamise_arv){
+    console.log(`Tõuse ja sära - ${kord}. kord`)
+        kord++
 }
 
-// treeningtüüp
-let minPulss;
-let maxPulss;
-
-if(treening == 1) {
-        minPulss = 0.5 * pulsisagedusMax;
-        maxPulss = 0.7 * pulsisagedusMax;
-} else if(treening == 2) {
-        minPulss = 0.7 * pulsisagedusMax;
-        maxPulss = 0.8 * pulsisagedusMax;
-} else if(treening == 3) {
-        minPulss = 0.8 * pulsisagedusMax;
-        maxPulss = 0.87 * pulsisagedusMax;
-}
-
-// ümardamine
-minPulss = Math.round(minPulss);
-maxPulss = Math.round(maxPulss);
-
-// output
-
-console.log("Pulsisagedus peaks olema vahemikus " + minPulss + " kuni " + maxPulss);
 
 
 
