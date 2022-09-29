3033d7745e06553fb95a398d03263879cb5d6266
raulerik <raul.erik.laur@gmail.com>
2022-09-29 13:38:53 +0300

    Ülesanne 3.3

--- datatypes/app.js
+++ datatypes/app.js
@@ -1,17 +1,11 @@
 // kasutaja sisend
-let ringideArv = Number(prompt("Sisesta ringide arv"));
-let porgandidKokku = 0;
+let taringuteArv = Number(prompt("Täringute arv"));
 
-let ring = 1;
-while(ring <= ringideArv){
-
-    if (ring % 2 == 0){
-        porgandidKokku = porgandidKokku + ring
-    }
-    ring++
+for (let kord = 1; kord <= taringuteArv; kord++){
+    let taring = Math.ceil(Math.random() * 6)
+    console.log(taring)
 }
 
-console.log(`Porgandite koguarv on ${porgandidKokku}.`)
 
 
 
