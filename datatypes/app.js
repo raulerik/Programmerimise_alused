f06b43f24f83d2db65bca44b21955f37705e49de
raulerik <raul.erik.laur@gmail.com>
2022-11-05 00:44:22 +0200

    Ülesanne 2.4

--- datatypes/app.js
+++ datatypes/app.js
@@ -1,17 +1,33 @@
-// kasutaja sisend
-let poialpoisid = Number(prompt("Mitu pöialpoissi tahab õunu?"));
-
-let lumivalgekeOunad = 14
-let poialpoiss = 1;
-while(poialpoiss <= poialpoisid){
-    let ounad = Math.ceil(Math.random() * 2)
-    console.log(ounad)
-    lumivalgekeOunad = lumivalgekeOunad - ounad
-    poialpoiss++
+let istekoht = prompt('Kas soovite valida ("ise") või loosida ("loos")?')
+while(istekoht !== 'ise' && istekoht !== 'loos'){
+    istekoht = prompt('Kas soovite valida ("ise") või loosida ("loos")?')
 }
-console.log(`Lumivalgekesele jäi ${lumivalgekeOunad}`)
-
-
+if(istekoht === 'ise'){
+    let kasAknasIste = prompt('Kas soovite istuda akna ääres("aken") või mitte("muu")?')
+    while(kasAknasIste !== 'aken' && kasAknasIste !== 'muu'){
+        kasAknasIste = prompt('Kas soovite istuda akna ääres("aken") või mitte("muu")?')
+    }
+    switch (kasAknasIste) {
+        case "aken":
+            console.log('Valisite ise. Aknakoht!')
+            break
+        case "muu":
+            console.log('Valisite ise. Vahekäigukoht!')
+            break
+    }
+}
+if(istekoht==='loos'){
+    let randInt = Math.floor(Math.random() *3   );
+    switch (randInt) {
+        case 0:
+        case 1:
+            console.log('Istekoht loositi. Vahekäigukoht!')
+            break
+        case 2:
+            console.log('Istekoht loositi. Aknakoht!')
+            break
+    }
+}
 
 
 
