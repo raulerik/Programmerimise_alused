1f810d34e3efd2405642175a3465dc38ab389ce5
raulerik <raul.erik.laur@gmail.com>
2022-11-06 18:22:32 +0200

    Ülesanne 2.5

--- datatypes/app.js
+++ datatypes/app.js
@@ -1,33 +1,25 @@
-let istekoht = prompt('Kas soovite valida ("ise") või loosida ("loos")?')
-while(istekoht !== 'ise' && istekoht !== 'loos'){
-    istekoht = prompt('Kas soovite valida ("ise") või loosida ("loos")?')
-}
-if(istekoht === 'ise'){
-    let kasAknasIste = prompt('Kas soovite istuda akna ääres("aken") või mitte("muu")?')
-    while(kasAknasIste !== 'aken' && kasAknasIste !== 'muu'){
-        kasAknasIste = prompt('Kas soovite istuda akna ääres("aken") või mitte("muu")?')
-    }
-    switch (kasAknasIste) {
-        case "aken":
-            console.log('Valisite ise. Aknakoht!')
-            break
-        case "muu":
-            console.log('Valisite ise. Vahekäigukoht!')
-            break
-    }
-}
-if(istekoht==='loos'){
-    let randInt = Math.floor(Math.random() *3   );
-    switch (randInt) {
-        case 0:
-        case 1:
-            console.log('Istekoht loositi. Vahekäigukoht!')
-            break
-        case 2:
-            console.log('Istekoht loositi. Aknakoht!')
-            break
-    }
-}
+let kirjaTeema = prompt('Sisestage kirja teema pealkiri:');
+let kirjaSuurus;
+let kirjaLisa;
 
+function isFloat(value) {
+    if (
+        typeof value === 'number' &&
+        !Number.isNaN(value)
+    ) {
+        return true;
+    }
+    return false;
+}
+while (!isFloat(kirjaSuurus)){
+    kirjaSuurus = parseFloat(prompt('Sisestage kirja suurus:'))
+}
+while (kirjaLisa !== 'jah' && kirjaLisa !== 'ei'){
+    kirjaLisa= prompt('Kas kirjaga on kaasas fail?')
+}
+if(kirjaTeema.length ===0 || (kirjaLisa === 'jah' && kirjaSuurus > 1.0)){
+    console.log('Kiri on spämm.')
+}
+else console.log('Kiri ei ole spämm.');
 
 
